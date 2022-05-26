40f181c8c359f501d9047e4e2ea0a947ee95cf64
Rocky.C <cloki0610@gmail.com>
2022-05-26 18:05:14 +0000

    Add Skeleton loading effect

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,15 +1,36 @@
-import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, Stack, Skeleton } from "@mui/material";
 
 import Post from "./Post";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Box flex={4} p={2}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+    <Box flex={4} p={{ xs: 0, md: 2 }}>
+      {loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="rectangular" height={300} />
+        </Stack>
+      ) : (
+        <>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </>
+      )}
     </Box>
   );
 };
