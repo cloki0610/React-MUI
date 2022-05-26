bcde721d7af26d2cac0d41f7d7a2ca960bd8931c
Rocky.C <cloki0610@gmail.com>
2022-05-26 17:39:53 +0000

    Complete Add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import { Box, Stack } from "@mui/material";
+
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import Navbar from "./components/Navbar";
-import { Box, Stack } from "@mui/material";
+import Add from "./components/Add";
 
 const App = () => {
   const [mode, setMode] = useState<string>("light");
   return (
     <Box>
       <Navbar />
-      <Stack direction="row" spacing={2} justify-content="space-between">
+      <Stack direction="row" spacing={1} justify-content="center">
         <Sidebar setMode={setMode} mode={mode} />
         <Feed />
         <Rightbar />
       </Stack>
+      <Add />
     </Box>
   );
 };
---
+++ src/components/Add.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import {
+  Fab,
+  Modal,
+  Tooltip,
+  Box,
+  Typography,
+  Avatar,
+  TextField,
+  Stack,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import styled from "@emotion/styled";
+import {
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+}));
+
+const Add = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Tooltip
+        title="Delete"
+        onClick={(e) => setOpen(true)}
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="grey" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="https://i.imgur.com/bt3nvMM.jpg"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} sx={{ display: "inline-block" }}>
+              Loki.C
+            </Typography>
+          </UserBox>
+          <TextField
+            id="outlined-multiline-static"
+            multiline
+            rows={3}
+            placeholder="What's on your mind?"
+            sx={{ width: "100%" }}
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions />
+            <Image />
+            <VideoCameraBack />
+            <PersonAdd />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
+    </>
+  );
+};
+
+export default Add;
