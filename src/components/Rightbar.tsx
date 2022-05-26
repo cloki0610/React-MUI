918ab769f88fe2fafc4f586dfbad7f6bfb88d724
Rocky.C <cloki0610@gmail.com>
2022-05-26 13:33:08 +0000

    Complete Rightbar

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,9 +1,121 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Avatar,
+  AvatarGroup,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Divider,
+  ListItemText,
+} from "@mui/material";
 
 const Rightbar = () => {
   return (
     <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <Box position="fixed"></Box>
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar alt="Remy Sharp" src="" />
+          <Avatar alt="Travis Howard" src="" />
+          <Avatar alt="Cindy Baker" src="" />
+          <Avatar alt="Agnes Walker" src="" />
+          <Avatar alt="Trevor Henderson" src="" />
+          <Avatar alt="Loki Gray" src="" />
+          <Avatar alt="Samuel Wong" src="" />
+          <Avatar alt="Yugiri" src="" />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img src="https://i.imgur.com/DHF14ZO.jpg?1" alt="shareimg1" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://i.imgur.com/4NY1Qhe.jpg?1" alt="shareimg2" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://i.imgur.com/xjBf8Jd.jpg?1" alt="shareimg3" />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2}>
+          Latest Conversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
